85ee1c6a2c812a5813ad1f20c5ae2eeedb964549
minifjurt123 <Fabian.Hedin@gmail.com>
2016-09-18 13:16:12 +0200

    requirejs

---
+++ app/main.js
@@ -0,0 +1,9 @@
+define(function (require) {
+    // Load any app-specific modules
+    // with a relative require call,
+    // like:
+    var init = require('./init');
+    var keyHandlers = require('./keyHandler');
+    var map = require('./map');
+    var viewPoint = require('./viewPoint');
+});
